88a003dba6d69ee8c0dfb80f5f0c2e107122cf4f
irwansaifranto <irwansaifranto93@gmail.com>
2020-06-03 04:53:21 +0700

    add class for global response => BaseResponse

---
+++ Service/Service.Models/Models/BaseResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Models.Models
+{
+    public class BaseResponse
+    {
+        public string Version { get; set; }
+        public string Datetime { set; get; }
+        public long Timestamp { get; set; }
+        public string Status { set; get; }
+        public int Code { set; get; }
+        public string Message { set; get; }
+        public dynamic Data { set; get; }
+        public dynamic Errors { set; get; }
+        public BaseResponse()
+        {
+            DateTime now = DateTime.UtcNow;
+
+            Version = "1.0";
+            Datetime = now.ToString("u");
+            Timestamp = ((DateTimeOffset)now).ToUnixTimeSeconds();
+            Status = "Error";
+            Code = 400;
+            Message = "Error";
+            Data = null;
+            Errors = null;
+        }
+    }
+}
